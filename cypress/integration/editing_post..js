e47928a907527773b2811bd7e0ab7fda7f25192c
juliettswifty <yuliakatmuse@gmail.com>
2018-04-26 12:08:03 +0300

    ✅ test editing post

---
+++ cypress/integration/editing_post..js
@@ -0,0 +1,27 @@
+describe('Editing post test', () => {
+  it('Editing Post', () => {
+    cy.visit('/movies/');
+
+    cy
+      .get('div')
+      .first()
+      .click();
+    cy.get('a').click();
+
+    cy.get('input[name=title]').type('Test title');
+
+    cy
+      .get('input[name=poster]')
+      .type(
+        'https://ia.media-imdb.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg'
+      );
+
+    cy.get('textarea[name=description]').type('Test description');
+
+    cy.get('input[name=year]').type('Test year');
+
+    cy.get('input[name=director]').type('Test director');
+
+    cy.get('button').click();
+  });
+});
