8954419b3eef6cfdd74688fd1d8dc46777e5e223
juliettswifty <yuliakatmuse@gmail.com>
2018-04-27 11:56:51 +0300

    💄  fixed propTypes

--- app/common/components/Checkbox/index.js
+++ app/common/components/Checkbox/index.js
@@ -1,21 +1,23 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import withStyles from 'withStyles';
 import { compose } from 'recompose';
 
 import styles from './styles.scss';
 
-const Checkbox = ({ label, id, ...rest }) => (
-  <Fragment>
-    <input className={styles.checkbox} type="checkbox" id={id} {...rest} />
-    <label className={styles.label} htmlFor={id}>
+const Checkbox = ({ label, ...rest }) => (
+  <React.Fragment>
+    <label className={styles.label}>
+      <input className={styles.checkbox} type="checkbox" {...rest} />
       {label}
     </label>
-  </Fragment>
+  </React.Fragment>
 );
 
 Checkbox.propTypes = {
   checked: PropTypes.bool,
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string,
 };
 
 export default compose(withStyles(styles))(Checkbox);
