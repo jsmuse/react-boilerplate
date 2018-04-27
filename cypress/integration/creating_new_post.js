cda5506527c7f844b29b8810c47012b486400cd9
juliettswifty <yuliakatmuse@gmail.com>
2018-04-27 10:41:43 +0300

    🐛  fixes in test

--- cypress/integration/creating_new_post.js
+++ cypress/integration/creating_new_post.js
@@ -1,24 +1,21 @@
-/* describe('Create a new post test', () => {
+describe('Create a new post test', () => {
   it('Creating a New Post', () => {
     cy.visit('/movies/create');
 
-    cy.get('input[name=title]')
-      .type('Test title');
+    cy.get('input[name=title]').type('Test title');
 
-    cy.get('input[name=poster]')
-      .type('https://ia.media-imdb.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg');
+    cy
+      .get('input[name=poster]')
+      .type(
+        'https://ia.media-imdb.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg'
+      );
 
-    cy.get('textarea[name=description]')
-      .type('Test description');
+    cy.get('textarea[name=description]').type('Test description');
 
-    cy.get('input[name=year]')
-      .type('Test year');
+    cy.get('input[name=year]').type('2000');
 
-    cy.get('input[name=director]')
-      .type('Test director');
+    cy.get('input[name=director]').type('Test director');
 
-    cy.get('button')
-      .click();
+    cy.get('button').click();
   });
 });
-*/
--- cypress/integration/editing_post..js
+++ cypress/integration/editing_post..js
@@ -8,19 +8,32 @@ describe('Editing post test', () => {
       .click();
     cy.get('a[name=edit]').click();
 
-    cy.get('input[name=title]').type('Test title');
+    cy
+      .get('input[name=title]')
+      .clear()
+      .type('Test title');
 
     cy
       .get('input[name=poster]')
+      .clear()
       .type(
         'https://ia.media-imdb.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg'
       );
 
-    cy.get('textarea[name=description]').type('Test description');
+    cy
+      .get('textarea[name=description]')
+      .clear()
+      .type('Test description');
 
-    cy.get('input[name=year]').type('Test year');
+    cy
+      .get('input[name=year]')
+      .clear()
+      .type('2000');
 
-    cy.get('input[name=director]').type('Test director');
+    cy
+      .get('input[name=director]')
+      .clear()
+      .type('Test director');
 
     cy.get('button').click();
   });
